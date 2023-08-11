6d03a64354903eed6e6f1d01a7708c77adfaf495
jjehyun <povt99@gmail.com>
2023-08-11 19:26:14 +0900

    chore CiCD 테스트

--- next.config.js
+++ next.config.js
@@ -7,10 +7,15 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['.'],
   },
+  images: {
+    unoptimized: true,
+  },
   poweredByHeader: false,
   basePath: '',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  target: 'server',
+  output: 'export',
 });
